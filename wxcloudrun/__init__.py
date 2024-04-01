3089b34686f41fe3b443889823c59de1b4837108
b0shen <2130556775@qq.com>
2024-04-01 16:58:00 +0800

    China3DV_v1

--- wxcloudrun/__init__.py
+++ wxcloudrun/__init__.py
@@ -2,13 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
 import config
-from flask import Flask, render_template
+
 # 因MySQLDB不支持Python3，使用pymysql扩展库代替MySQLDB库
 pymysql.install_as_MySQLdb()
 
 # 初始化web应用
-app = Flask(__name__, static_folder='../static',  #设置静态文件夹目录
-instance_relative_config=True)
+app = Flask(__name__,
+static_folder='../static',  #设置静态文件夹目录
+template_folder = "../static",
+static_url_path="",  instance_relative_config=True)
 app.config['DEBUG'] = config.DEBUG
 
 
